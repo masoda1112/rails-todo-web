c5546c55e191be71b47d99c6e4a7ffa4311627a8
masoda1112 <wendy.odakura@gmail.com>
2021-06-14 20:19:26 +0900

    createPage修正

--- src/layouts/createPage.js
+++ src/layouts/createPage.js
@@ -2,9 +2,9 @@ import CreateForm from "../components/createForm";
 
 const CreatePage = () => {
   return (
-    <>
+    <div className="createPage">
       <CreateForm />
-    </>
+    </div>
   );
 };
 export default CreatePage;
